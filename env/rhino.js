1d5c790f0234cc1bf606ab3c4f0e739bd68a347d
Anton Kovalyov <anton@kovalyov.net>
2011-01-30 15:35:32 -0800

    Added Rhino wrapper

---
+++ env/rhino.js
@@ -0,0 +1,28 @@
+load("jshint.js");
+
+(function (args) {
+    var name = args[0], input;
+
+    if (!name) {
+        print('Usage: jshint.js file.js');
+        quit(1);
+    }
+
+    input = readFile(name);
+
+    if (!input) {
+        print('jshint: Couldn\'t open file ' + name);
+        quit(1);
+    }
+
+    if (!JSHINT(input, { rhino: true })) {
+        for (var i = 0, err; err = JSHINT.errors[i]; i++) {
+            print(err.reason + ' (line: ' + err.line + ', character: ' + err.character + ')');
+            print('> ' + (err.evidence || '').replace(/^\s*(\S*(\s+\S+)*)\s*$/, "$1"));
+            print('');
+        }
+        quit(1);
+    }
+
+    quit(0); 
+}(arguments));
