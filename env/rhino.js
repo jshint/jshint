34f762c3e727f15bc4b9df006135cab698b5cc64
Mike Bulman <mbulman@gmail.com>
2011-02-18 18:10:12 -0600

    Allow specifying options via rhino command line

--- env/rhino.js
+++ env/rhino.js
@@ -3,11 +3,21 @@
 load("jshint.js");
 
 (function (args) {
-    var name = args[0], input;
+    var name = args[0],
+        optstr = args[1], // arg1=val1,arg2=val2,...
+        opts = {rhino: true},
+        input, optname, optval;
 
     if (!name) {
         print('Usage: jshint.js file.js');
         quit(1);
+    }
+
+    if (optstr) {
+        optstr.split(',').forEach(function(arg) {
+            [optname,optval] = arg.split('=');
+            opts[optname] = optval;
+        });
     }
 
     input = readFile(name);
@@ -17,7 +27,7 @@ load("jshint.js");
         quit(1);
     }
 
-    if (!JSHINT(input, { rhino: true })) {
+    if (!JSHINT(input, opts)) {
         for (var i = 0, err; err = JSHINT.errors[i]; i++) {
             print(err.reason + ' (line: ' + err.line + ', character: ' + err.character + ')');
             print('> ' + (err.evidence || '').replace(/^\s*(\S*(\s+\S+)*)\s*$/, "$1"));
