b0ba5991358976e40b9c24ad523ed0eab33eb118
Ben Blank <ben.blank@gmail.com>
2011-04-17 17:15:07 -0700

    refactored wsh.js output formatting into a series of property formatters

--- env/wsh.js
+++ env/wsh.js
@@ -2,6 +2,60 @@
 /*global ActiveXObject: false, JSHINT: false, WScript: false */
 
 (function() {
+	var formatters = {
+		errors: function(errors, lines) {
+			for (var i = 0; i < errors.length; i++) {
+				var error = errors[i];
+
+				if (!error) continue;
+
+				if (i) lines.push("");
+
+				lines.push("Line " + error.line + " character " + error.character + ": " + error.reason);
+
+				if (error.evidence) lines.push("    " + error.evidence.replace(/^\s*((?:[\S\s]*\S)?)\s*$/, "$1"));
+			}
+		},
+
+		implieds: function(implieds, lines) {
+			lines.push("Implied globals:");
+
+			var globals = {};
+
+			for (var i = 0; i < implieds.length; i++) {
+				var item = implieds[i];
+
+				if (!(item.name in globals)) globals[item.name] = [];
+
+				globals[item.name].push(item.line);
+			}
+
+			for (var name in globals) {
+				lines.push("    " + name + ": " + globals[name].join(", "));
+			}
+		},
+
+		unused: function(unused, lines) {
+			lines.push("Unused variables:");
+
+			var func, names = {};
+
+			for (var i = 0; i < unused.length; i++) {
+				var item = unused[i];
+
+				func = item["function"];
+
+				if (!(func in names)) names[func] = [];
+
+				names[func].push(item.name + " (" + item.line + ")");
+			}
+
+			for (func in names) {
+				lines.push("    " + func + ": " + names[func].join(", "));
+			}
+		}
+	};
+
 	// load JSHint if the two scripts have not been concatenated
 	if (typeof JSHINT === "undefined") eval(new ActiveXObject("Scripting.FileSystemObject").OpenTextFile("..\\jshint.js", 1).ReadAll());
 
@@ -11,68 +65,23 @@
 	JSHINT(source.substr(0, source.length - 1), { passfail: false });
 
 	var data = JSHINT.data();
-	var exit = 0;
+	var lines = [];
 
-	if (data.errors && data.errors.length) {
-		exit = 1;
+	for (var formatter in formatters) {
+		if (data[formatter]) {
+			if (lines.length) lines.push("");
 
-		for (var i = 0; i < data.errors.length; i++) {
-			var error = data.errors[i];
-
-			if (!error) continue;
-
-			if (i) WScript.StdOut.WriteLine("");
-
-			WScript.StdOut.WriteLine("Lint at line " + error.line + " character " + error.character + ": " + error.reason);
-
-			if (error.evidence) WScript.StdOut.WriteLine("    " + error.evidence.replace(/^\s*((?:[\S\s]*\S)?)\s*$/, "$1"));
-		}
-
-		if ((data.unused && data.unused.length) || (data.implieds && data.implieds.length)) WScript.StdOut.WriteLine("");
-	}
-
-	if (data.unused && data.unused.length) {
-		exit = 1;
-
-		WScript.StdOut.WriteLine("Unused variables:");
-
-		var unused = {};
-
-		for (var i = 0; i < data.unused.length; i++) {
-			var item = data.unused[i];
-			var func = item["function"];
-
-			if (!(func in unused)) unused[func] = [];
-
-			unused[func].push(item.name + " (" + item.line + ")");
-		}
-
-		for (var func in unused) {
-			WScript.StdOut.WriteLine("    " + func + ": " + unused[func].join(", "));
-		}
-
-		if (data.implieds && data.implieds.length) WScript.StdOut.WriteLine("");
-	}
-
-	if (data.implieds && data.implieds.length) {
-		exit = 1;
-
-		WScript.StdOut.WriteLine("Implied globals:");
-
-		var implieds = {};
-
-		for (var i = 0; i < data.implieds.length; i++) {
-			var item = data.implieds[i];
-
-			if (!(item.name in implieds)) implieds[item.name] = [];
-
-			implieds[item.name].push(item.line);
-		}
-
-		for (var name in implieds) {
-			WScript.StdOut.WriteLine("    " + name + ": " + implieds[name].join(", "));
+			formatters[formatter](data[formatter], lines);
 		}
 	}
 
-	WScript.Quit(exit);
+	if (lines.length) {
+		for (var i = 0; i < lines.length; i++) {
+			WScript.StdOut.WriteLine(lines[i]);
+		}
+
+		WScript.Quit(1);
+	} else {
+		WScript.Quit(0);
+	}
 }());
