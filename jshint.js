18eba29bd369baebcbb88469226b9e5229f78454
Anton Kovalyov <anton@kovalyov.net>
2011-07-26 16:39:28 -0700

    Removed the concept of Too Many Errors; you can still use passfail and maxerr.
    Closes #180.

--- jshint.js
+++ jshint.js
@@ -806,8 +806,7 @@ var JSHINT = (function () {
     }
 
 
-// Produce an error warning.
-
+    // Produce an error warning.
     function quit(message, line, chr) {
         var percentage = Math.floor((line / lines.length) * 100);
 
@@ -859,7 +858,6 @@ var JSHINT = (function () {
 
     function error(m, t, a, b, c, d) {
         var w = warning(m, t, a, b, c, d);
-        quit("Stopping, unable to continue.", w.line, w.character);
     }
 
     function errorAt(m, l, ch, a, b, c, d) {
@@ -3688,9 +3686,7 @@ loop:   for (;;) {
         return JSHINT.errors.length === 0;
     };
 
-
-// Data summary.
-
+    // Data summary.
     itself.data = function () {
 
         var data = {functions: []}, fu, globals, implieds = [], f, i, j,
