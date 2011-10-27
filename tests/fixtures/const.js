0f5731448294b13ee6221a5386cd8b388e6016c8
Anton Kovalyov <anton@kovalyov.net>
2011-10-27 15:57:20 -0700

    Style-related changes to the GH-319 (esnext option)

--- tests/fixtures/const.js
+++ tests/fixtures/const.js
@@ -1,59 +1,57 @@
-/*global require */
+/*
+ * The (approved) proposal for const in Harmony wiki:
+ *   http://wiki.ecmascript.org/doku.php?id=harmony:const
+ *
+ * These tests reflect v8's current implementation of const they can be ran in `node` or they
+ * can ran by using the test runner.
+ */
 
-// The Proposal: http://wiki.ecmascript.org/doku.php?id=harmony:const
-
-/**
-  These tests reflect v8's current implementation of const
-  they can be ran in `node`
-  or they can ran by using the test runner.
-  */
 function assert (a, b) {
-  if (a !== b) {
-    console.log("fail:", a, b);
-  }
+    if (a !== b) {
+        console.log("fail:", a, b);
+    }
 }
 
-// setting up the tests
+// Setting up the tests.
 const immutable = "foo";
 const immutable2 = 6;
 const immutable3 = false;
 
-// perfectly valid but useless
+// Perfectly valid but useless.
 const immutable4;
 
-// objects aren't immutable
-// one can modify it's properties
-// but can't redeclare as something other than this object
+// Objects are mutable: one can modify it's properties but can't redeclare as something other than
+// this object.
 const immutable5 = { foo: "bar" };
 immutable5.foo = "not bar";
 
-// functions can be constant
+// Functions can be constant
 const immutable6 = function () {
   return "foo";
 };
 
-// arrays aren't immutable either
-// same rules apply as an Object
+// Arrays are mutable as well, with the same rules apply as for Object.
 const immutable7 = ["foo"];
 immutable7.pop();
 
-// comma separated is ok
+// Comma separated is ok.
 const immutable8 = "testing", immutable9 = true;
 
-// in current implementations, const doesn't have block scope
+// In current implementations, const doesn't have block scope.
 for (var i = 0; i < 1; i += 1) {
-  // const immutable9 = false; // :(
+  // :(
+  // const immutable9 = false;
 }
 
-// in scope
+// In scope
 (function () {
-  // valid
-  const immutable4 = "foobar"; // immutable4 is in it's own scope so it can be redeclared
+  // Valid
+  const immutable4 = "foobar"; // immutable4 is in it's own scope so it can be redeclared.
   const immutable5 = "w00t";
 
   const immutable6 = true, immutable7 = ["bar"];
 
-  // tests
+  // Tests
   assert(immutable3, false);
   assert(immutable4, "foobar");
   assert(immutable5, "w00t");
@@ -62,7 +60,7 @@ for (var i = 0; i < 1; i += 1) {
   assert(immutable7.length, 0);
 }());
 
-// tests
+// Tests
 assert(immutable, "foo");
 assert(immutable2, 6);
 assert(immutable3, false);
