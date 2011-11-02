9cbec1eb55402bd0a0827f886db2dcaa67f1a496
Anton Kovalyov <anton@kovalyov.net>
2011-11-02 11:51:25 -0700

    Revised Node.js global (per v0.5.9) and added a test case for ECMAScript standard globals (no option needed).
    Closes GH-287. Related to GH-284.

--- jshint.js
+++ jshint.js
@@ -571,16 +571,20 @@ var JSHINT = (function () {
         nexttoken,
 
         node = {
-            __filename  : false,
-            __dirname   : false,
-            Buffer      : false,
-            console     : false,
-            exports     : false,
-            GLOBAL      : false,
-            global      : false,
-            module      : false,
-            process     : false,
-            require     : false
+            __filename    : false,
+            __dirname     : false,
+            Buffer        : false,
+            console       : false,
+            exports       : false,
+            GLOBAL        : false,
+            global        : false,
+            module        : false,
+            process       : false,
+            require       : false,
+            setTimeout    : false,
+            clearTimeout  : false,
+            setInterval   : false,
+            clearInterval : false
         },
 
         noreach,
