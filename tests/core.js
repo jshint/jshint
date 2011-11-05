0d3fba7c9045e4aa362926cf123d6deb00cac838
Josh Perez <josh@goatslacker.com>
2011-11-04 23:05:00 -0700

    Adds jQuery 1.7 library to tests

--- tests/core.js
+++ tests/core.js
@@ -255,3 +255,35 @@ exports.globalDeclarations = function () {
 
     TestRun().test(src.join('\n'));
 };
+
+exports.testJQuery = function () {
+    var src = fs.readFileSync(__dirname + '/fixtures/jquery-1.7.js', 'utf8');
+
+    TestRun()
+        .addError(2818, "Expected an assignment or function call and instead saw an expression.")
+        .addError(2822, "Expected an assignment or function call and instead saw an expression.")
+        .addError(4560, "Expected an assignment or function call and instead saw an expression.")
+        .addError(4702, "Mixed spaces and tabs.")
+        .addError(4712, "Expected a 'break' statement before 'case'.")
+        .addError(4715, "Mixed spaces and tabs.")
+        .addError(4843, "Expected an assignment or function call and instead saw an expression.")
+        .addError(6912, "Mixed spaces and tabs.")
+        .addError(6913, "Mixed spaces and tabs.")
+        .addError(6914, "Mixed spaces and tabs.")
+        .addError(6915, "Mixed spaces and tabs.")
+        .addError(6916, "Mixed spaces and tabs.")
+        .addError(6917, "Mixed spaces and tabs.")
+        .addError(6918, "Mixed spaces and tabs.")
+        .addError(6919, "Mixed spaces and tabs.")
+        .addError(6923, "Mixed spaces and tabs.")
+        .addError(6924, "Mixed spaces and tabs.")
+        .addError(6925, "Mixed spaces and tabs.")
+        .addError(6926, "Mixed spaces and tabs.")
+        .addError(8086, "Mixed spaces and tabs.")
+        .addError(8087, "Mixed spaces and tabs.")
+        .addError(8088, "Mixed spaces and tabs.")
+        .addError(8089, "Mixed spaces and tabs.")
+        .addError(8090, "Mixed spaces and tabs.")
+        .addError(9209, "Mixed spaces and tabs.")
+        .test(src);
+};
