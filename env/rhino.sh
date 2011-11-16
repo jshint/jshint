682c68569c3dc48f6144e443e4d8c2ee07441989
Larry Myers <larry@larrymyers.com>
2011-11-16 11:52:19 -0500

    include usage example for rhino script runner

--- env/rhino.sh
+++ env/rhino.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# USAGE: rhino.sh [jshint options] [filenames]
+
+# Example: rhino.sh forin=true,noarg=true,noempty=true,eqeqeq=true,bitwise=true,undef=true,curly=true,browser=true,indent=4,maxerr=50 myFile.js
+
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 java -jar $DIR/js.jar $DIR/jshint-rhino.js $DIR $*
