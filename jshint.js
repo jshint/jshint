5f88eaba97be3b4283b1ac45291f5584076c6c75
Aparajita Fishman <aparajita@aparajita.com>
2011-11-27 18:26:07 -0800

    Fix unclosed regular expression error

--- jshint.js
+++ jshint.js
@@ -1387,7 +1387,7 @@ unclosedString:     for (;;) {
                                     case '':
                                         errorAt("Unclosed regular expression.",
                                                 line, from);
-                                        return;
+                                        return quit('Stopping.', line, from);
                                     case '/':
                                         if (depth > 0) {
                                             warningAt("{a} unterminated regular expression " +
