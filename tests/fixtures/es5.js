c7b3d9ef1d7e49f6f9093dac0e2d95d1fb468c83
Anton Kovalyov <anton@kovalyov.net>
2011-12-09 15:36:12 -0800

    Change how JSHint treats ES5 accessors.
    Don't require getter *and* setter, but show a warning when setter
    is used without a corresponding getter.
    
    Fixes GH-373
    Fixes GH-374

--- tests/fixtures/es5.js
+++ tests/fixtures/es5.js
@@ -15,4 +15,24 @@ var b = {
         get x() { return _x; },
         set x(value) { _x = value; }
     };
+
+    var obj2 = {
+        get x() { return _x; },
+        name: 'jshint',
+        set x(value) { _x = value; }
+    };
+
+    var onlyGetter1 = {
+        get x() { return _x; }
+    };
+
+    var onlyGetter2 = {
+        get x() { return _x; },
+        get y() { return _x; },
+        a: 1
+    };
+
+    var onlySetter = {
+        set x(value) { _x = value; }
+    };
 }());
