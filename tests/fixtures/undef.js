c7213f2ce25a790c1ccac155b868b14af813d763
Mike Pennisi <mike@mikepennisi.com>
2012-01-02 18:07:27 -0500

    Ensure that null references are not subscripted within 'typeof' and 'delete' operators

--- tests/fixtures/undef.js
+++ tests/fixtures/undef.js
@@ -2,9 +2,15 @@ undef(); // this line will generate a warning
 if (typeof undef) {} // this line won't because typeof accepts a reference
                      // even when the base object of that reference is null
 
+if (typeof undef['attr' + 0]) {}
+if (typeof undef.attr) {}
+
 var fn = function () {
     localUndef();
 
     if (typeof localUndef)
         return;
+
+    if (typeof localUndef['attr' + 0]) {}
+    if (typeof localUndef.attr) {}
 };
