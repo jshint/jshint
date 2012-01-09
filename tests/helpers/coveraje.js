712c63da5928f73a9de30efb9f4fa18ed6758a2e
Josh Perez <josh@goatslacker.com>
2012-01-08 16:36:00 -0800

    Removes shebang and unecessary whitespace.

--- tests/helpers/coveraje.js
+++ tests/helpers/coveraje.js
@@ -1,15 +1,13 @@
-#!/usr/bin/env node
-
 /*jshint node: true*/
 
 (function () {
     "use strict";
-    
+
     var coveraje = require("coveraje").coveraje,
         fs = require("fs"),
         path = require("path");
     var filePath = path.resolve(path.join(__dirname, "../../jshint.js"));
-        
+
     function runTests(file, helper) {
         return function (context) {
             // hack: set cache value
@@ -18,39 +16,39 @@
                 exports: context.exports,
                 loaded: true
             };
-            
+
             return coveraje.runHelper(helper).run(file);
         };
     }
-    
+
     var tests = {};
     var rd = path.join(__dirname, "..");
-    
+
     function collect(dirs) {
         var l = dirs.length;
         var r = 0;
-        
+
         function onDir(dirPath) {
             return function (err, files) {
                 r += 1;
-                
+
                 if (!err) {
                     files.forEach(function (f) {
                         var p = path.join(dirPath, f);
                         var stat;
-                        
+
                         try {
                             stat = fs.statSync(p);
                         } catch (ex) {
                             return;
                         }
-                        
+
                         if (stat.isFile()) {
                             tests[path.relative(rd, p)] = runTests(path.join(dirPath, f), "expresso");
                         }
                     });
                 }
-                
+
                 if (r === l) { // all dirs loaded
                     coveraje.cover(
                         function () {
@@ -68,17 +66,17 @@
                         }
                     );
                 }
-                
+
             };
         }
-        
-        
+
+
         for (var i = 0; i < l; i += 1) {
             var subPath = path.join(rd, dirs[i]);
-            
+
             fs.readdir(subPath, onDir(subPath));
         }
     }
-    
+
     collect(["unit", "regression"]);
 }());
