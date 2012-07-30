8d018a709e998b5abb4d06fc1188a572259b40a0
Anton Kovalyov <anton@kovalyov.net>
2012-07-30 15:00:48 -0700

    Make quotmark to work via /*jshint ... */ syntax.
    Apparently, the newly introduced quotmark option doesn't work
    via /*jshint ... */ syntax which is bad because all options
    must be settable through all channels.
    
    This patch fixes that.

---
+++ tests/unit/fixtures/quotes3.js
@@ -0,0 +1,34 @@
+(function () {
+    /*jshint quotmark:double */
+
+    var test1 = 'hello';
+    var test2 = "hello";
+})();
+
+(function () {
+    /*jshint quotmark:single */
+
+    var test1 = 'hello';
+    var test2 = "hello";
+})();
+
+(function () {
+    /*jshint quotmark:true */
+
+    var test1 = 'hello';
+    var test2 = "hello";
+})();
+
+(function () {
+    /*jshint quotmark:false */
+
+    var test1 = 'hello';
+    var test2 = "hello";
+})();
+
+(function () {
+    /*jshint quotmark:bad */
+
+    var test1 = 'hello';
+    var test2 = "hello";
+})();
