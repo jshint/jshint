b8f430df0d28c39882b656cc526afe78dc527ff3
Anton Kovalyov <anton@kovalyov.net>
2012-08-20 10:57:02 -0700

    Add a shim for Array.prototype.indexOf
    Apparently, whatever JavaScript engine WSH uses, it doesn't support
    Array.prototype.indexOf method so I had to add a shim for it. I took
    the original version from MDN and modified it to pass our lint tests.
    
    Closes GH-617.

--- jshint.js
+++ jshint.js
@@ -186,7 +186,7 @@
  Timer, Tips, Type, TypeError, Toggle, Try, "use strict", unescape, URI, URIError, URL,
  VBArray, WSH, WScript, XDomainRequest, Web, Window, XMLDOM, XMLHttpRequest, XMLSerializer,
  XPathEvaluator, XPathException, XPathExpression, XPathNamespace, XPathNSResolver, XPathResult,
- "\\", a, addEventListener, address, alert, apply, applicationCache, arguments, arity,
+ "\\", a, abs, addEventListener, address, alert, apply, applicationCache, arguments, arity,
  asi, atob, b, basic, basicToken, bitwise, blacklist, block, blur, boolOptions, boss,
  browser, btoa, c, call, callee, caller, camelcase, cases, charAt, charCodeAt, character,
  clearInterval, clearTimeout, close, closed, closure, comment, condition, confirm, console,
@@ -202,7 +202,7 @@
  isDigit, isFinite, isNaN, iterator, java, join, jshint,
  JSHINT, json, jquery, jQuery, keys, label, labelled, last, lastcharacter, lastsemic, laxbreak,
  laxcomma, latedef, lbp, led, left, length, line, load, loadClass, localStorage, location,
- log, loopfunc, m, match, maxerr, maxlen, member,message, meta, module, moveBy,
+ log, loopfunc, m, match, max, maxerr, maxlen, member,message, meta, module, moveBy,
  moveTo, mootools, multistr, name, navigator, new, newcap, noarg, node, noempty, nomen,
  nonew, nonstandard, nud, onbeforeunload, onblur, onerror, onevar, onecase, onfocus,
  onload, onresize, onunload, open, openDatabase, openURL, opener, opera, options, outer, param,
@@ -830,6 +830,44 @@ var JSHINT = (function () {
             for (var i = 0; i < len; i++) {
                 fn.call(scope || this, this[i], i, this);
             }
+        };
+    }
+
+    if (!Array.prototype.indexOf) {
+        Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
+            if (this === null || this === undefined) {
+                throw new TypeError();
+            }
+
+            var t = new Object(this);
+            var len = t.length >>> 0;
+
+            if (len === 0) {
+                return -1;
+            }
+
+            var n = 0;
+            if (arguments.length > 0) {
+                n = Number(arguments[1]);
+                if (n != n) { // shortcut for verifying if it's NaN
+                    n = 0;
+                } else if (n !== 0 && n != Infinity && n != -Infinity) {
+                    n = (n > 0 || -1) * Math.floor(Math.abs(n));
+                }
+            }
+
+            if (n >= len) {
+                return -1;
+            }
+
+            var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);
+            for (; k < len; k++) {
+                if (k in t && t[k] === searchElement) {
+                    return k;
+                }
+            }
+
+            return -1;
         };
     }
 
