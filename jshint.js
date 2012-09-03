655920993c52cad4a91dd3d25b2a94e47ac0f039
Anton Kovalyov <anton@kovalyov.net>
2012-09-02 20:09:22 -0700

    Break early when { is not followed by a number.
    When parsing regular expressions and encountering a '{' that is
    not followed by a number there is absolutely no reason to continue
    parsing it as if there was a number.
    
    Closes GH-538.

--- jshint.js
+++ jshint.js
@@ -1759,6 +1759,7 @@ klass:                                  do {
                                             if (c < "0" || c > "9") {
                                                 warningAt(
 "Expected a number and instead saw '{a}'.", line, from + l, c);
+                                                break; // No reason to continue checking numbers.
                                             }
                                             l += 1;
                                             low = +c;
