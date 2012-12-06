423a1d5179b72f37a6d975c663ce920fc001ca3e
Mike MacCana <mike.maccana@bazaarvoice.com>
2012-12-06 13:26:32 -0800

    Make 'escapement' errors use the term 'escaping' instead.
    Also clarified the language re: 'bad for in variables' to mention
    why they're bad and added a test case for them.
    
    Closes GH-736.
    Closes GH-747.

--- src/stable/reg.js
+++ src/stable/reg.js
@@ -14,7 +14,7 @@ exports.unsafeString =
 exports.unsafeChars =
 	/[\u0000-\u001f\u007f-\u009f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/;
 
-// Characters in strings that need escapement (nx and nxg)
+// Characters in strings that need escaping (nx and nxg)
 exports.needEsc =
 	/[\u0000-\u001f&<"\/\\\u007f-\u009f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/;
 
