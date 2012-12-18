54a4850fdbe492d5ee1894589249ef6a2c92cc7c
Anton Kovalyov <anton@kovalyov.net>
2012-12-17 19:15:16 -0800

    Added 'verbose' option to the CLI.
    For now it simply shows message code alongside the message text.
    
    Fixes GH-706.

--- src/reporters/default.js
+++ src/reporters/default.js
@@ -1,15 +1,23 @@
 "use strict";
 
 module.exports = {
-	reporter: function (results) {
+	reporter: function (results, data, opts) {
 		var len = results.length;
 		var str = '';
+
+		opts = opts || {};
 
 		results.forEach(function (result) {
 			var file = result.file;
 			var error = result.error;
 			str += file  + ': line ' + error.line + ', col ' +
-				error.character + ', ' + error.reason + '\n';
+				error.character + ', ' + error.reason;
+
+			if (opts.verbose) {
+				str += ' (' + error.code + ')';
+			}
+
+			str += '\n';
 		});
 
 		if (str) {
