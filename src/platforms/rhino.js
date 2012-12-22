c9e274d05e53bd72b6d8eb11124459399f592293
Anton Kovalyov <anton@kovalyov.net>
2012-12-21 18:40:15 -0800

    New dist builder, JSC is out.
    This commit changes our builder to produce three versions of JSHint:
    
     1. Browserified version.
     2. Browserified+Rhino version. (Requires Rhino 1.7R4!)
     3. Browserified+WSH version.
    
    JavaScript Core support is out because it doesn't have any usable
    APIs.
    
    Closes GH-697.

--- src/platforms/rhino.js
+++ src/platforms/rhino.js
@@ -1,8 +1,9 @@
 /*jshint boss: true, rhino: true, unused: true, undef: true, white: true, quotmark: double */
-/*global JSHINT*/
 
 (function (args) {
 	"use strict";
+
+	var JSHINT = require("/src/stable/jshint.js").JSHINT;
 
 	var filenames = [];
 	var optstr; // arg1=val1,arg2=val2,...
