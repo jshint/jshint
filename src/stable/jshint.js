58f8c575efbb240ca0c4e5bbd9250695ba4a78f9
idearat <ss@technicalpursuit.com>
2013-01-10 15:09:54 -0700

    Adjust spaces to tabs.

--- src/stable/jshint.js
+++ src/stable/jshint.js
@@ -68,7 +68,7 @@ var JSHINT = (function () {
 
 		// These are the JSHint boolean options.
 		boolOptions = {
-            anon        : true, // allow immediate left-paren after "function".
+			anon        : true, // allow immediate left-paren after "function".
 			asi         : true, // if automatic semicolon insertion should be tolerated
 			bitwise     : true, // if bitwise operators should not be allowed
 			boss        : true, // if advanced usage of assignments should be allowed
@@ -2637,10 +2637,10 @@ var JSHINT = (function () {
 		if (i) {
 			adjacent(state.tokens.curr, state.tokens.next);
 		} else {
-            if (option.anon) {
-			    adjacent(state.tokens.curr, state.tokens.next);
+			if (option.anon) {
+				adjacent(state.tokens.curr, state.tokens.next);
             } else {
-			    nonadjacent(state.tokens.curr, state.tokens.next);
+				nonadjacent(state.tokens.curr, state.tokens.next);
             }
 		}
 		doFunction(i);
