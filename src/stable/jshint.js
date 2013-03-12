d02401a80e1228d5e1de06867f48345ceb4dbaf5
Guyzmo <guyzmo+github@m0g.net>
2013-03-12 12:09:46 +0100

    fixed block `let() {}` versus expression `let()` determination ; added tests

--- src/stable/jshint.js
+++ src/stable/jshint.js
@@ -777,7 +777,8 @@ var JSHINT = (function () {
 		var left, isArray = false, isObject = false, isLetExpr = false;
 
 		// if current expression is a let expression
-		if (state.option.esnext && state.tokens.next.value === "let" && peek(0).value === "(") {
+		if (state.option.esnext && 
+				!initial && state.tokens.next.value === "let" && peek(0).value === "(") {
 			isLetExpr = true;
 			// create a new block scope we use only for the current expression
 			funct["(blockscope)"].stack();
@@ -785,9 +786,6 @@ var JSHINT = (function () {
 			advance("(");
 			state.syntax["let"].fud.call(state.syntax["let"].fud, false);
 			advance(")");
-			if (state.tokens.next.value === "{") {
-				block(true);
-			}
 		}
 
 		if (state.tokens.next.id === "(end)")
@@ -2798,9 +2796,12 @@ var JSHINT = (function () {
 		});
 		varstatement.exps = true;
 		var letstatement = stmt("let", function (prefix) {
-			// JavaScript does not have block scope. It only has function scope. So,
-			// declaring a variable in a block can have unexpected consequences.
-			var tokens, lone, value;
+			var tokens, lone, value, letblock;
+
+			if (state.tokens.next.value === "(") {
+				advance("(");
+				letblock = true;
+			}
 
 			if (funct["(onevar)"] && state.option.onevar) {
 				warning("W081");
@@ -2861,6 +2862,12 @@ var JSHINT = (function () {
 				}
 				comma();
 			}
+			if (letblock) {
+				advance(")");
+				block(true, true);
+				this.block = true;
+			}
+			
 			return this;
 		});
 		letstatement.exps = true;
