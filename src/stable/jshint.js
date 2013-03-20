ab98371d1cdc5b1380457d63679dfd4feaacc24d
Bernard `Guyzmo` Pratz <guyzmo+github@m0g.net>
2013-03-20 23:50:36 +0100

    added function* support with esnext option, and errors on function* with moz option
    Signed-off-by: Bernard `Guyzmo` Pratz <guyzmo+github@m0g.net>

--- src/stable/jshint.js
+++ src/stable/jshint.js
@@ -2415,7 +2415,7 @@ var JSHINT = (function () {
 	}
 
 
-	function doFunction(name, statement) {
+	function doFunction(name, statement, generator) {
 		var f;
 		var oldOption = state.option;
 		var oldScope  = scope;
@@ -2437,6 +2437,10 @@ var JSHINT = (function () {
 			"(blockscope)": funct["(blockscope)"],
 			"(comparray)" : funct["(comparray)"]
 		};
+
+		if (generator) {
+			funct["(generator)"] = true;
+		}
 
 		f = funct;
 		state.tokens.curr.funct = funct;
@@ -3031,6 +3035,15 @@ var JSHINT = (function () {
 	varstatement.exps = true;
 
 	blockstmt("function", function () {
+		var generator = false;
+		if (state.tokens.next.value === "*") {
+			advance("*");
+			if (state.option.esnext && !state.option.moz) {
+				generator = true;
+			} else {
+				warning("W119", state.tokens.curr, "function*");
+			}
+		}
 		if (inblock) {
 			warning("W082", state.tokens.curr);
 
@@ -3042,7 +3055,7 @@ var JSHINT = (function () {
 		adjacent(state.tokens.curr, state.tokens.next);
 		addlabel(i, "unction", state.tokens.curr);
 
-		doFunction(i, { statement: true });
+		doFunction(i, { statement: true }, generator);
 		if (state.tokens.next.id === "(" && state.tokens.next.line === state.tokens.curr.line) {
 			error("E039");
 		}
@@ -3050,13 +3063,18 @@ var JSHINT = (function () {
 	});
 
 	prefix("function", function () {
+		var generator = false;
+		if (state.option.esnext && state.tokens.next.value === "*") {
+			advance("*");
+			generator = true;
+		}
 		var i = optionalidentifier();
 		if (i || state.option.gcl) {
 			adjacent(state.tokens.curr, state.tokens.next);
 		} else {
 			nonadjacent(state.tokens.curr, state.tokens.next);
 		}
-		doFunction(i);
+		doFunction(i, undefined, generator);
 		if (!state.option.loopfunc && funct["(loopage)"]) {
 			warning("W083");
 		}
@@ -3560,7 +3578,9 @@ var JSHINT = (function () {
 	}).exps = true;
 
 	stmt("yield", function () {
-		if (!state.option.moz) {
+		if (state.option.esnext && funct["(generator)"] !== true) {
+			error("E046", state.tokens.curr, "yield")
+		} else if (!isMozOrESNext()) {
 			warning("W104", state.tokens.curr, "yield");
 		}
 		if (this.line === state.tokens.next.line) {
