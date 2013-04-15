23627ffac413472985d328650a41c0c99cbbabe7
Bernard `Guyzmo` Pratz <guyzmo+github@m0g.net>
2013-04-15 16:52:26 +0200

    Fixed issue #1010 that allows statements with commas.
    Signed-off-by: Bernard `Guyzmo` Pratz <guyzmo+github@m0g.net>

--- src/stable/jshint.js
+++ src/stable/jshint.js
@@ -1081,7 +1081,7 @@ var JSHINT = (function () {
 			case "while":
 			case "with":
 				error("E024", state.tokens.next, state.tokens.next.value);
-				return;
+				return false;
 			}
 		}
 
@@ -1091,14 +1091,16 @@ var JSHINT = (function () {
 			case "]":
 			case ",":
 				if (opts.allowTrailing) {
-					return;
+					return true;
 				}
 
 				/* falls through */
 			case ")":
 				error("E024", state.tokens.next, state.tokens.next.value);
+				return false;
 			}
 		}
+		return true;
 	}
 
 	// Functional constructors for making the symbols that will be inherited by
@@ -1554,8 +1556,12 @@ var JSHINT = (function () {
 				warning("W031", t);
 			}
 
-			if (state.tokens.next.id === ",") {
-				return comma();
+			while (state.tokens.next.id === ",") {
+				if (comma()) {
+					r = expression(0, true);
+				} else {
+					return;
+				}
 			}
 
 			if (state.tokens.next.id !== ";") {
