d6756abec9623dd2732c69b0c7629090a32dbb0c
usrbincc <usrbincc@yahoo.com>
2013-05-01 13:03:38 -0400

    Forgot to make class bodies strict.

--- src/stable/jshint.js
+++ src/stable/jshint.js
@@ -3224,6 +3224,7 @@ var JSHINT = (function () {
 
 	function classtail(c) {
 		var strict = state.directive["use strict"];
+		state.directive["use strict"] = true;
 		var ce;
 		// ClassHeritage(opt)
 		if (state.tokens.next.value === "extends") {
