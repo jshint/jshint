54c3c5630109c865d5ed3314ab8e2de099b1fb4e
Anton Kovalyov <anton@kovalyov.net>
2013-08-02 13:30:33 -0700

    No issue: Remove make.js lint in favor of npm test

--- make.js
+++ make.js
@@ -19,66 +19,6 @@ target.all = function () {
 	target.lint();
 };
 
-target.lint = function () {
-	var jshint = require("jshint").JSHINT;
-	var files = find("src")
-		.filter(function (file) {
-			return file.match(/\.js$/);
-		})
-		.concat(
-			ls(__dirname + "/*.js").filter(function (file) {
-				return file !== __dirname + "/demo.js";
-			})
-		);
-
-	TESTS.forEach(function (dir) {
-		ls(dir + "*.js").forEach(function (file) {
-			files.push(file);
-		});
-	});
-
-	echo("Linting files...", "\n");
-
-	var failures = {};
-	files.forEach(function (file) {
-		var passed = jshint(cat(file), OPTIONS);
-		process.stdout.write(passed ? "." : "F");
-
-		if (!passed) {
-			failures[file] = jshint.data();
-		}
-	});
-
-	echo("\n");
-
-	if (Object.keys(failures).length === 0) {
-		cli.ok("All files passed.");
-		echo("\n");
-		return;
-	}
-
-	var outputError = function (err) {
-		if (!err) {
-			return;
-		}
-
-		var line = "[L" + err.line + "]";
-		while (line.length < 10) {
-			line += " ";
-		}
-
-		echo(line, err.reason);
-		echo("\n");
-	};
-
-	for (var key in failures) {
-		cli.error(key);
-		failures[key].errors.forEach(outputError);
-	}
-
-	exit(1);
-};
-
 target.build = function () {
 	var browserify = require("browserify");
 	var bundle = browserify("./src/stable/jshint.js");
