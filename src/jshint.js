bb8c421e8c8aa3b5838f86919a9ce477a445e58c
Jared Jacobs <jared@42go.com>
2013-10-20 16:40:31 -0700

    Fixed #1293: Make latedef=nofunc ignore inner functions.
    Signed-off-by: Anton Kovalyov <anton@kovalyov.net>

--- src/jshint.js
+++ src/jshint.js
@@ -1561,11 +1561,9 @@ var JSHINT = (function () {
 			}
 			if (t.id !== "(endline)") {
 				if (t.id === "function") {
-					if (!state.option.latedef) {
-						break;
+					if (state.option.latedef === true) {
+						warning("W026", t);
 					}
-
-					warning("W026", t);
 					break;
 				}
 
