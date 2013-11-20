9051d1db939363c3a0264afe73886bbfc0581d86
Travis Kaufman <tkaufman@appnexus.com>
2013-11-20 16:54:36 -0500

    Add concise method support for functions name 'get' and 'set' This patch modifies getter and setter checks so that if the esnext flag is enabled jshint will not treat functions named 'get' and 'set' as getters/setters (respectively) *unless* there is a property name specified after it. For example:
        var ins = [];
        var outs = [];
        var SimpleMap = {
          get(key) {
            return outs[ins.indexOf(key)] || null;
          },
          set(key, value) {
            ins.push(key);
            outs.push(value);
          },
          has(key) {
            return ins.indexOf(key) >= 0;
          },
          delete(key) {
            var idx = this.indexOf(key);
            if (idx >= 0) {
              ins.splice(idx, 1);
              outs.splice(idx, 1);
            }
          }
        };
    
    If one were to run this code through jshint with esnext enabled, it would
    complain about the `get` and `set` methods, even though this is perfectly valid
    es6 code. This patch fixes this, but still complains when esnext is not
    enabled.

--- src/jshint.js
+++ src/jshint.js
@@ -3035,7 +3035,11 @@ var JSHINT = (function () {
 					}
 
 					i = property_name();
-					if (!i) {
+
+					// ES6 allows for get() {...} and set() {...} method
+					// definition shorthand syntax, so we don't produce an error
+					// if the esnext option is enabled.
+					if (!i && !state.option.inESNext()) {
 						error("E035");
 					}
 
@@ -3045,13 +3049,19 @@ var JSHINT = (function () {
 						error("E049", state.tokens.next, "class getter method", i);
 					}
 
-					saveGetter(tag + i);
+					// We don't want to save this getter unless it's an actual getter
+					// and not an ES6 concise method
+					if (i) {
+						saveGetter(tag + i);
+					}
+
 					t = state.tokens.next;
 					adjacent(state.tokens.curr, state.tokens.next);
 					f = doFunction();
 					p = f["(params)"];
 
-					if (p) {
+					// Don't warn about getter/setter pairs if this is an ES6 concise method
+					if (i && p) {
 						warning("W076", t, p[0], i);
 					}
 
@@ -3064,7 +3074,11 @@ var JSHINT = (function () {
 					}
 
 					i = property_name();
-					if (!i) {
+
+					// ES6 allows for get() {...} and set() {...} method
+					// definition shorthand syntax, so we don't produce an error
+					// if the esnext option is enabled.
+					if (!i && !state.option.inESNext()) {
 						error("E035");
 					}
 
@@ -3074,13 +3088,19 @@ var JSHINT = (function () {
 						error("E049", state.tokens.next, "class setter method", i);
 					}
 
-					saveSetter(tag + i, state.tokens.next);
+					// We don't want to save this getter unless it's an actual getter
+					// and not an ES6 concise method
+					if (i) {
+						saveSetter(tag + i, state.tokens.next);
+					}
+
 					t = state.tokens.next;
 					adjacent(state.tokens.curr, state.tokens.next);
 					f = doFunction();
 					p = f["(params)"];
 
-					if (!p || p.length !== 1) {
+					// Don't warn about getter/setter pairs if this is an ES6 concise method
+					if (i && (!p || p.length !== 1)) {
 						warning("W077", t, i);
 					}
 				} else {
