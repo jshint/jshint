8e0975b86f14908ad2af4b599493737fffb1622d
Nikhil Benesch <nikhil.benesch@gmail.com>
2013-12-20 12:22:14 -0500

    Remove overzealous W092 (wrap regexp in parens after return stmt)
    This warning flags any bare regexp after a return or yield statment as
    ambiguous. (Example: `return /[0-9]/`.) The unnecessary fix is to wrap
    the regexp in parens like so:
    
        return (/[0-9]/)
    
    This is a useless warning. An unwrapped regexp following a return
    statement is not ambiguous to humans or parsers. The division operator
    (/) must follow an expression; a regexp literal must *not* follow an
    expression. This warning flags regexp literals that appear immediately
    after a return statement and thus never after an expression.
    
    No reason to optionalize this warning, as it's not useful.
    
    References:
      http://groups.yahoo.com/neo/groups/jslint_com/conversations/topics/345
      nzakas/eslint#324
      jshint/jshint#245
      jshint/jshint#1218

--- src/jshint.js
+++ src/jshint.js
@@ -4060,9 +4060,6 @@ var JSHINT = (function () {
 
 	stmt("return", function () {
 		if (this.line === state.tokens.next.line) {
-			if (state.tokens.next.id === "(regexp)")
-				warning("W092");
-
 			if (state.tokens.next.id !== ";" && !state.tokens.next.reach) {
 				nonadjacent(state.tokens.curr, state.tokens.next);
 				this.first = expression(0);
@@ -4095,9 +4092,6 @@ var JSHINT = (function () {
 		}
 		funct["(generator)"] = "yielded";
 		if (this.line === state.tokens.next.line || !state.option.inMoz(true)) {
-			if (state.tokens.next.id === "(regexp)")
-				warning("W092");
-
 			if (state.tokens.next.id !== ";" && !state.tokens.next.reach && state.tokens.next.nud) {
 				nobreaknonadjacent(state.tokens.curr, state.tokens.next);
 				this.first = expression(10);
