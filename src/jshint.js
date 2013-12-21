fd4d847896211c82cf7f87d724cad62f8eda6e95
Anton Kovalyov <anton@kovalyov.net>
2013-12-20 18:32:09 -0800

    Issue #1375: Make sure _.zip always receives an array

--- src/jshint.js
+++ src/jshint.js
@@ -3257,19 +3257,20 @@ var JSHINT = (function () {
 		return identifiers;
 	}
 	function destructuringExpressionMatch(tokens, value) {
-		if (value.first) {
-			_.zip(tokens, value.first).forEach(function (val) {
-				var token = val[0];
-				var value = val[1];
-				if (token && value) {
-					token.first = value;
-				} else if (token && token.first && !value) {
-					warning("W080", token.first, token.first.value);
-				} /* else {
-					XXX value is discarded: wouldn't it need a warning ?
-				} */
-			});
-		}
+		var first = value.first;
+
+		if (!first)
+			return;
+
+		_.zip(tokens, Array.isArray(first) ? first : [ first ]).forEach(function (val) {
+			var token = val[0];
+			var value = val[1];
+
+			if (token && value)
+				token.first = value;
+			else if (token && token.first && !value)
+				warning("W080", token.first, token.first.value);
+		});
 	}
 
 	var conststatement = stmt("const", function (prefix) {
