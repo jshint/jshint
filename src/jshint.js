e7c0133cb8ce89921bf5b8883877170170a7bc96
Julien Wajsberg <felash@gmail.com>
2014-01-02 22:34:01 +0100

    fix issue #1388 using another way

--- src/jshint.js
+++ src/jshint.js
@@ -1704,6 +1704,14 @@ var JSHINT = (function () {
 		}
 		r = expression(0, true);
 
+		if (r && (!r.identifier || r.value !== "function") && (r.type !== "(punctuator)")) {
+			if (!state.directive["use strict"] &&
+					state.option.globalstrict &&
+					state.option.strict) {
+				warning("E007");
+			}
+		}
+
 		// Look for the final semicolon.
 
 		if (!t.block) {
