85b5b59474d278a0afa85c3a0f0ff125038fcd05
Mark S. Everitt <mark.s.everitt@gmail.com>
2014-01-03 12:56:26 -0800

    Issue #1123: Make checks for generator functions without yield optional
    Signed-off-by: Anton Kovalyov <anton@kovalyov.net>

--- src/messages.js
+++ src/messages.js
@@ -62,7 +62,7 @@ var errors = {
 	E044: null,
 	E045: "Invalid for each loop.",
 	E046: "A yield statement shall be within a generator function (with syntax: `function*`)",
-	E047: "A generator function shall contain a yield statement.",
+	E047: null, // Vacant
 	E048: "Let declaration not directly within block.",
 	E049: "A {a} cannot be named '{b}'.",
 	E050: "Mozilla requires the yield expression to be parenthesized here.",
@@ -194,7 +194,8 @@ var warnings = {
 	W120: "You might be leaking a variable ({a}) here.",
 	W121: "Extending prototype of native object: '{a}'.",
 	W122: "Invalid typeof value '{a}'",
-	W123: "'{a}' is already defined in outer scope."
+	W123: "'{a}' is already defined in outer scope.",
+	W124: "A generator function shall contain a yield statement."
 };
 
 var info = {
