e74ff4d374d50ce07347791b2ca2d9406854682c
Anton Kovalyov <anton@kovalyov.net>
2014-01-25 18:08:13 -0800

    No issue: tabs to spaces

--- examples/reporter.js
+++ examples/reporter.js
@@ -1,21 +1,21 @@
 "use strict";
 
 module.exports = {
-	reporter: function (res) {
-		var len = res.length;
-		var str = "";
+  reporter: function (res) {
+    var len = res.length;
+    var str = "";
 
-		res.forEach(function (r) {
-			var file = r.file;
-			var err = r.error;
+    res.forEach(function (r) {
+      var file = r.file;
+      var err = r.error;
 
-			str += file + ": line " + err.line + ", col " +
-				err.character + ", " + err.reason + "\n";
-		});
+      str += file + ": line " + err.line + ", col " +
+        err.character + ", " + err.reason + "\n";
+    });
 
-		if (str) {
-			process.stdout.write(str + "\n" + len + " error" +
-				((len === 1) ? "" : "s") + "\n");
-		}
-	}
+    if (str) {
+      process.stdout.write(str + "\n" + len + " error" +
+        ((len === 1) ? "" : "s") + "\n");
+    }
+  }
 };
