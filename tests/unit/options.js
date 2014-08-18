48803062c441d02a8098ce0739d6d418a83e92c1
Paul Wade <paul.wade@live.com>
2014-08-17 21:10:21 -0500

    #1603 adding new option unit test and fixture file to test "-globaloptioname" in predef

--- tests/unit/options.js
+++ tests/unit/options.js
@@ -1637,3 +1637,11 @@ exports.enforceall = function (test) {
 
   test.done();
 };
+
+exports.removeglobal = function (test) {
+    var src = fs.readFileSync(__dirname + '/fixtures/removeglobals.js', 'utf8');
+    TestRun(test)
+        .addError(1,"'JSON' is not defined.")
+        .test(src, {undef: true, predef: ["-JSON","myglobal"]});
+    test.done();
+};
