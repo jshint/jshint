b0027a7a1c0d09c12942beeaf20aafd4706b3a3d
Paul Wade <paul.wade@live.com>
2014-08-18 10:10:12 -0500

    fix indentation and change to double quotes
    Also fixed the enforceall test I made a while back to use double quotes.

--- tests/unit/options.js
+++ tests/unit/options.js
@@ -1622,7 +1622,7 @@ exports.nonbsp = function (test) {
 };
 
 exports.enforceall = function (test) {
-  var src = fs.readFileSync(__dirname + '/fixtures/enforceall.js', 'utf8');
+  var src = fs.readFileSync(__dirname + "/fixtures/enforceall.js", "utf8");
 
   // Throws errors not normally on be default
   TestRun(test)
@@ -1639,11 +1639,11 @@ exports.enforceall = function (test) {
 };
 
 exports.removeglobal = function (test) {
-  var src = fs.readFileSync(__dirname + '/fixtures/removeglobals.js', 'utf8');
+  var src = fs.readFileSync(__dirname + "/fixtures/removeglobals.js", "utf8");
 
-    TestRun(test)
-    .addError(1,"'JSON' is not defined.")
-    .test(src, {undef: true, predef: ["-JSON","myglobal"]});
+  TestRun(test)
+    .addError(1, "'JSON' is not defined.")
+    .test(src, { undef: true, predef: ["-JSON", "myglobal"] });
 
   test.done();
 };
