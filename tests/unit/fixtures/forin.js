710ad1666670480cab27f6c025dd02ae44c0a32d
Johannes Marbach <JohannesMarbach@gmail.com>
2014-09-04 19:53:34 +0200

    Make 'forin' check recognize negative checks with continue
    This patch modifies the 'forin' check so that it correctly recognizes
    negative conditional checks using continue. For example:
    
    for (var key in collection) {
        if (! collection.hasOwnProperty(key)) {
            continue;
        }
        doSomething();
    }
    
    The conditional itself is not analyzed beyond checking whether it
    starts with a negation operator or not. Results of the if analysis
    are stored in a stack that is evaluated after the for loop was parsed
    completely. As a result, the improved check also works for nested loops.
    
    References:
        Closes GH-229

--- tests/unit/fixtures/forin.js
+++ tests/unit/fixtures/forin.js
@@ -4,6 +4,52 @@ for (var key in objects) {
   }
 }
 
+for (var key in objects) {
+  if (! objects.hasOwnProperty(key)) {
+    continue;
+  }
+  hey();
+}
+
+// More statements in loop body than just the if
+for (var key in objects) {
+  if (objects.hasOwnProperty(key)) {
+    hey();
+  }
+  hey();
+}
+
+// No continue
+for (var key in objects) {
+  if (! objects.hasOwnProperty(key)) {
+    hey();
+  }
+  hey();
+}
+
+// Nested loops
+for (var key in objects) {
+  if (! objects.hasOwnProperty(key)) {
+    continue;
+  }
+  
+  // No if statement
+  for (var key2 in objects) {
+    hey();
+    
+    for (var key3 in objects) {
+      if (objects.hasOwnProperty(key3)) {
+        // No continue
+        for (var key4 in objects) {
+          if (! objects.hasOwnProperty(key4)) {
+            hey();
+          }
+        }
+      }
+    }
+  }
+}
+
 var hasOwn = Object.prototype.hasOwnProperty;
 
 for (var p in o) {
@@ -25,6 +71,5 @@ for ( key in objects ) { }
 
 // Let's make sure we continue scanning the rest of the file.
 for (key in objects) {
-    hey();
+  hey();
 }
-
