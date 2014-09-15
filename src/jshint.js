9a3b6b9f66222ffaa6f4465cfb22a3322661aa3a
Mike Pennisi <mike@mikepennisi.com>
2014-09-15 18:07:37 -0400

    Allow paren-wrapped function literals with `immed`
    Function literals must be wrapped in parenthesis if their properties are
    to be accessed. Ensure that this syntax does not trigger warnings when
    the `immed` option is set.
    
    The native Function prototype may be extended with any arbitrary
    property, making the current property whitelisting approach
    inappropriate.
    
    Closes gh-1737

--- src/jshint.js
+++ src/jshint.js
@@ -2609,7 +2609,7 @@ var JSHINT = (function () {
     advance(")", this);
     if (state.option.immed && exprs[0] && exprs[0].id === "function") {
       if (state.tokens.next.id !== "(" &&
-        (state.tokens.next.id !== "." || (peek().value !== "call" && peek().value !== "apply"))) {
+        (state.tokens.next.id !== ".")) {
         warning("W068", this);
       }
     }
