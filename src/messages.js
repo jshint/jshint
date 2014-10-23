092f06fdabb82eaa5cc234d0d50da291be89af87
Mike Pennisi <mike@mikepennisi.com>
2014-10-23 19:06:26 -0400

    fixup! Correct typo

--- src/messages.js
+++ src/messages.js
@@ -200,7 +200,7 @@ var warnings = {
   W123: "'{a}' is already defined in outer scope.",
   W124: "A generator function shall contain a yield statement.",
   W125: "This line contains non-breaking spaces: http://jshint.com/doc/options/#nonbsp",
-  W126: "Grouping operator is unecessary for lone expressions."
+  W126: "Grouping operator is unnecessary for lone expressions."
 };
 
 var info = {
