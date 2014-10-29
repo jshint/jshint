b5dd259283f8dad4eb8fbad4ef3f22756cc57e40
Mike Pennisi <mike@mikepennisi.com>
2014-10-29 19:46:04 -0400

    Allow string literal dereferencing as first stmt
    Fixes gh-1937

--- src/jshint.js
+++ src/jshint.js
@@ -1807,6 +1807,8 @@ var JSHINT = (function () {
           // string -> ( is not a valid production
           warning("W033", state.tokens.next);
         }
+      } else if (p.id === "." || p.id === "[") {
+        return;
       } else if (p.id !== ";") {
         warning("W033", p);
       }
