aad906254f65ac44bb41196953faf42a7d1b17f8
Andy Wingo <wingo@igalia.com>
2014-11-02 22:53:00 -0500

    Updates to yield delegation linting. Fixes gh-1544

--- tests/unit/fixtures/yield-expressions.js
+++ tests/unit/fixtures/yield-expressions.js
@@ -82,4 +82,5 @@ function* G() {
     yield ? yield : yield;
     yield ? yield ? yield : yield : yield;
     yield ? yield ? yield : yield : yield ? yield : yield;
+    yield* (function*(){})();
 }
