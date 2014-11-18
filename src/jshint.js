2efb880e295d11d4f394524a8866f581fd5847e7
Caitlin Potter <caitpotter88@gmail.com>
2014-11-17 22:11:59 -0500

    Don't warn fat arrow params used out of scope. Fixes #1983
    This is a hack, but I couldn't find a quicker fix.

--- src/jshint.js
+++ src/jshint.js
@@ -1923,7 +1923,9 @@ var JSHINT = (function () {
         case "function":
         case "var":
         case "unused":
-          warning("W038", state.tokens.curr, v);
+          if (!state.parsingFatArrowParams) {
+            warning("W038", state.tokens.curr, v);
+          }
           break;
         case "label":
           warning("W037", state.tokens.curr, v);
@@ -2472,7 +2474,9 @@ var JSHINT = (function () {
             exprs.push(bracket.left[t].token);
           }
         } else {
+          state.parsingFatArrowParams = pn1.value === "=>";
           exprs.push(expression(10));
+          state.parsingFatArrowParams = false;
         }
         if (state.tokens.next.id !== ",") {
           break;
