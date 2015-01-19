996b42e25ae60d7d010879815642f35269a8ca1e
Mike Pennisi <mike@mikepennisi.com>
2015-01-19 00:38:31 -0500

    [[FIX]] Define "build" module
    Factoring the build logic into a standalone Node.js module will allow
    programmatically building temporary versions of JSHint for testing
    purposes.

---
+++ scripts/build.js
@@ -0,0 +1,45 @@
+"use strict";
+
+var browserify = require("browserify");
+var path       = require("path");
+var version    = require("../package.json").version;
+
+var srcDir     = path.join(__dirname, "../src");
+
+var targets = ["web", "rhino"];
+
+module.exports = function(target, done) {
+  var bundle = browserify();
+
+  if (targets.indexOf(target) === -1) {
+    done(new Error("Unrecognized target: '" + target + "'"));
+    return;
+  }
+
+  bundle.require(srcDir + "/jshint.js", { expose: "jshint" });
+  bundle.bundle(function(err, src) {
+    var wrapped;
+
+    if (err) {
+      done(err);
+      return;
+    }
+
+    wrapped = [ "/*! " + version + " */",
+      "var JSHINT;",
+      "if (typeof window === 'undefined') window = {};",
+      "(function () {",
+        "var require;",
+        src,
+        "JSHINT = require('jshint').JSHINT;",
+        "if (typeof exports === 'object' && exports) exports.JSHINT = JSHINT;",
+      "}());"
+    ];
+
+    if (target === "rhino") {
+      wrapped.splice(0, 0, "#!/usr/bin/env rhino", "var window = {};");
+    }
+
+    done(null, version, wrapped.join("\n"));
+  });
+};
