b40e4063b5174be626e53cfe9f5113b4b8ce515a
Mike Pennisi <mike@mikepennisi.com>
2015-01-31 14:16:44 -0500

    [[FIX]] Correct behavior of `singleGroups`
    Parsing logic can generally infer if the current token begins an
    expression by referencing the `left` property of the previous token. The
    `singleGroups` option relies on this behavior in order to recognize
    situations where operator binding power makes the grouping operator
    necessary.
    
    Parsing of the "addition" operator should be made consistent with the
    other infix operators: the token's `left` property should be set *prior
    to* parsing the right-hand-side expression.

--- src/jshint.js
+++ src/jshint.js
@@ -2165,7 +2165,10 @@ var JSHINT = (function() {
   infix("in", "in", 120);
   infix("instanceof", "instanceof", 120);
   infix("+", function(left, that) {
-    var right = expression(130);
+    var right;
+    that.left = left;
+    that.right = right = expression(130);
+
     if (left && right && left.id === "(string)" && right.id === "(string)") {
       left.value += right.value;
       left.character = right.character;
@@ -2174,8 +2177,7 @@ var JSHINT = (function() {
       }
       return left;
     }
-    that.left = left;
-    that.right = right;
+
     return that;
   }, 130);
   prefix("+", "num");
