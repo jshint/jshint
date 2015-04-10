262649e1b7e82b00a2b3882af17f210c3aa054b3
Sergey Belov <peimei@ya.ru>
2015-04-10 22:03:46 +0300

    Replace underscore with lodash

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var _                 = require("underscore");
+var _                 = require("lodash");
 var fs                = require("fs");
 var cli               = require("cli");
 var path              = require("path");
@@ -533,9 +533,11 @@ var exports = {
 
       if (config['extends']) {
         var baseConfig = exports.loadConfig(path.resolve(config.dirname, config['extends']));
-        config.globals = _.extend({}, baseConfig.globals, config.globals);
-        config.overrides = _.extend({}, baseConfig.overrides, config.overrides);
-        _.defaults(config, baseConfig);
+        config = _.merge({}, baseConfig, config, function(a, b) {
+          if (_.isArray(a)) {
+            return a.concat(b);
+          }
+        });
         delete config['extends'];
       }
 
