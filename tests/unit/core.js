a51f91e88efd51d628f228f9da9a88609981c01e
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2015-10-10 14:09:58 +0200

    test destructuring default value

--- tests/unit/core.js
+++ tests/unit/core.js
@@ -775,6 +775,10 @@ exports.testUndefinedAssignment = function (test) {
     "var l = undefined === 0;",
     "const m = undefined === 0;",
     "let n = undefined === 0;",
+    "let [ o = undefined === 0 ] = [];",
+    "[ o = undefined === 0] = [];",
+    "let { p = undefined === 0, x: q = undefined === 0 } = {};",
+    "({ p = undefined === 0, x: q = undefined === 0 } = {});"
   ];
 
   TestRun(test)
