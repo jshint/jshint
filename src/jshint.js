4067ae6386cd4d94648ff727c80c2c7d6946d12a
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2015-12-07 17:10:59 +0100

    [[FIX]] Make the 'freeze' option less strict
    Don't warn when the prototype of a local variable
    named as a native object is modified.
    
    Fixes gh-1600

--- src/jshint.js
+++ src/jshint.js
@@ -1300,8 +1300,10 @@ var JSHINT = (function() {
       while (!obj.identifier && typeof obj.left === "object")
         obj = obj.left;
 
-      if (obj.identifier && natives.indexOf(obj.value) >= 0)
+      if (obj.identifier && natives.indexOf(obj.value) >= 0 &&
+          state.funct["(scope)"].isPredefined(obj.value)) {
         return obj.value;
+      }
     }
 
     var prototype = walkPrototype(left);
