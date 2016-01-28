f63e45a0b9d6e8436acf069e784c096294b1c1f5
C. Scott Ananian <cscott@cscott.net>
2016-01-28 16:57:36 -0500

    [[FIX]] Save Rhino global object `this` in `global` variable
    When the Rhino JavaScript interpreter executes scripts, `this` is
    set to the global context object.  This fixes a regression in
    the jshint-rhino.js file (broken in JSHint 2.7.0).
    
    Fixes gh-2308

--- scripts/build.js
+++ scripts/build.js
@@ -40,7 +40,7 @@ module.exports = function(target, done) {
     ];
 
     if (target === "rhino") {
-      wrapped.splice(0, 0, "#!/usr/bin/env rhino", "var window = {};");
+      wrapped.splice(0, 0, "#!/usr/bin/env rhino", "var global = this;");
     }
 
     done(null, version, wrapped.join("\n"));
