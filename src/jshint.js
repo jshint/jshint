a16590a3a0b31e8a59a6d2e31b2e65efda07f40f
LMLB <LMLB@users.noreply.github.com>
2016-08-28 10:19:34 +0200

    Replace Private Use Area character
    fixes #3018

--- src/jshint.js
+++ src/jshint.js
@@ -4661,7 +4661,7 @@ var JSHINT = (function() {
 
     if (state.tokens.next.type === "default") {
       // ExportDeclaration ::
-      //      export default [lookahead  { function, class }] AssignmentExpression[In] ;
+      //      export default [lookahead ∉ { function, class }] AssignmentExpression[In] ;
       //      export default HoistableDeclaration
       //      export default ClassDeclaration
       state.nameStack.set(state.tokens.next);
