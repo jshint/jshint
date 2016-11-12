c0838666dd53ac40106208d3a3edf9a9f77e9b18
Mike Pennisi <mike@mikepennisi.com>
2016-11-12 14:11:11 -0500

    [[FIX]] Avoid crash when peeking past end of prog
    The internal `peek` function may be used to request tokens beyond the
    end of the program. Because multiple call sites expect the function to
    consistently return an object value, the "end of program" token is be
    returned in these cases.
    
    However, this situation may occur in cases where the "end of program"
    token is not the next token. The previous implementation of the function
    would produce the `null` value in this event, violating callers'
    expectation and resulting in runtime reference errors.
    
    Update the internal `peek` function to consistently return the "end of
    program" token when input has been exhausted--even when this is not the
    next token.

--- tests/unit/parser.js
+++ tests/unit/parser.js
@@ -8009,3 +8009,14 @@ exports.octalEscape = function (test) {
 
   test.done();
 };
+
+// See gh-3004, "Starting jsdoc comment causes 'Unclosed regular expression'
+// error"
+exports.lookaheadBeyondEnd = function (test) {
+  TestRun(test)
+    .addError(1, "Unmatched '{'.")
+    .addError(1, "Unrecoverable syntax error. (100% scanned).")
+    .test("({ a: {");
+
+  test.done();
+};
