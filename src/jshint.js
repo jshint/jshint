edb5789595f1ec3836b44a2f0e4220026dd9f50e
Mike Pennisi <mike@mikepennisi.com>
2017-05-06 17:26:47 -0400

    [[DOCS]] Improve warning message
    Describe the expected language feature more precisely based on the
    context in which the "..." is encountered.

--- src/jshint.js
+++ src/jshint.js
@@ -1497,16 +1497,18 @@ var JSHINT = (function() {
    * it is present. If the operator is repeated, consume every repetition, and
    * issue a single error describing the syntax error.
    *
+   * @param {string} operation - either "spread" or "rest"
+   *
    * @returns {boolean} a value describing whether or not any tokens were
    *                    consumed in this way
    */
-  function spreadrest() {
+  function spreadrest(operation) {
     if (!checkPunctuator(state.tokens.next, "...")) {
       return false;
     }
 
     if (!state.inES6(true)) {
-      warning("W119", state.tokens.next, "spread/rest operator", "6");
+      warning("W119", state.tokens.next, operation + " operator", "6");
     }
     advance();
 
@@ -2428,7 +2430,7 @@ var JSHINT = (function() {
 
     if (state.tokens.next.id !== ")") {
       for (;;) {
-        spreadrest();
+        spreadrest("spread");
 
         p[p.length] = expression(10);
         n += 1;
@@ -2752,7 +2754,7 @@ var JSHINT = (function() {
         break;
       }
 
-      spreadrest();
+      spreadrest("spread");
 
       this.first.push(expression(10));
       if (state.tokens.next.id === ",") {
@@ -2879,7 +2881,7 @@ var JSHINT = (function() {
           }
         }
       } else {
-        pastRest = spreadrest();
+        pastRest = spreadrest("rest");
         ident = identifier();
         if (ident) {
           paramsIds.push(ident);
@@ -3486,7 +3488,7 @@ var JSHINT = (function() {
       }
       var element_after_rest = false;
       while (!checkPunctuator(state.tokens.next, "]")) {
-        var isRest = spreadrest();
+        var isRest = spreadrest("rest");
 
         nextInnerDE();
 
