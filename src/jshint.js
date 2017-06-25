cc517ab60608b1a854a344403b6df70eb4171be1
Mike Pennisi <mike@mikepennisi.com>
2017-06-25 14:21:40 -0400

    [[FEAT]] Introduce linting option `leanswitch`

--- src/jshint.js
+++ src/jshint.js
@@ -4266,6 +4266,12 @@ var JSHINT = (function() {
         case "switch":
         case "throw":
           break;
+        case "default":
+          if (state.option.leanswitch) {
+            warning("W145", state.tokens.next);
+          }
+
+          break;
         default:
           // You can tell JSHint that you don't use break intentionally by
           // adding a comment /* falls through */ on a line just before
@@ -4290,6 +4296,12 @@ var JSHINT = (function() {
         case "return":
         case "throw":
           break;
+        case "case":
+          if (state.option.leanswitch) {
+            warning("W145", state.tokens.curr);
+          }
+
+          break;
         default:
           // Do not display a warning if 'default' is the first statement or if
           // there is a special /* falls through */ comment.
@@ -4303,6 +4315,7 @@ var JSHINT = (function() {
         advance("default");
         g = true;
         advance(":");
+        state.funct["(verb)"] = "default";
         break;
       case "}":
         if (!noindent)
