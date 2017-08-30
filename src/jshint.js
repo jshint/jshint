70d310dd0ef8e2bbe68315f873cfe9675cf0e87a
jshaptic <eugene.shatilo@gmail.com>
2017-08-31 00:13:46 +0300

    [[CHORE]] Remove unused property, `new`
    Property new is not used anywhere, can be removed

--- src/jshint.js
+++ src/jshint.js
@@ -2345,7 +2345,6 @@ var JSHINT = (function() {
     var c = expression(155), i;
     if (c && c.id !== "function") {
       if (c.identifier) {
-        c["new"] = true;
         switch (c.value) {
         case "Number":
         case "String":
