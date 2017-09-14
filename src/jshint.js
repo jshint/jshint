759ec9625301ff583e0eb3ce8f26cea9ae7dde9f
jshaptic <eugene.shatilo@gmail.com>
2017-09-15 00:15:45 +0300

    [[CHORE]] Removed unused property `tokens[0].first`
    Property `tokens[0].first` is not used anywhere in the code and can be
    removed.

--- src/jshint.js
+++ src/jshint.js
@@ -3548,9 +3548,7 @@ var JSHINT = (function() {
         if (!prefix && value && value.type === "undefined") {
           warning("W080", id, id.value);
         }
-        if (lone) {
-          tokens[0].first = value;
-        } else {
+        if (!lone) {
           destructuringPatternMatch(names, value);
         }
       }
@@ -3660,9 +3658,7 @@ var JSHINT = (function() {
         if (value && !prefix && !state.funct["(loopage)"] && value.type === "undefined") {
           warning("W080", id, id.value);
         }
-        if (lone) {
-          tokens[0].first = value;
-        } else {
+        if (!lone) {
           destructuringPatternMatch(names, value);
         }
       }
