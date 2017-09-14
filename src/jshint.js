827f5d28a3d830c207cbaf29c0b00443915c8ac1
jshaptic <eugene.shatilo@gmail.com>
2017-09-15 00:14:12 +0300

    [[CHORE]] Changed for loop to while
    There is no need to use `for` loop here because iterator variable is not
    used anywhere and condition doesn't depend on it

--- src/jshint.js
+++ src/jshint.js
@@ -3736,7 +3736,7 @@ var JSHINT = (function() {
     var props = Object.create(null);
     var staticProps = Object.create(null);
     var computed;
-    for (var i = 0; state.tokens.next.id !== "}"; ++i) {
+    while (state.tokens.next.id !== "}") {
       name = state.tokens.next;
       isStatic = false;
       isGenerator = false;
