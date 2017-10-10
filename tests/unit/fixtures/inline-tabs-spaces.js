4c0289522d393db5ff9894a88fbb4b8111277c9d
johnmryan <johnmryan830@gmail.com>
2017-10-09 23:35:40 -0700

    [[FIX]] Tolerate whitespace in inline directives

---
+++ tests/unit/fixtures/inline-tabs-spaces.js
@@ -0,0 +1,18 @@
+function a() {
+    //      jshint unused: true     
+    var x;
+}
+
+function b() {
+    /*      jshint  
+    unused: true
+    */
+    var y;
+}
+
+function c() {
+    /*      jshint      
+        unused: true
+    */
+    var z;
+}
