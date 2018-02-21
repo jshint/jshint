11653fa768ffed505a7b87692bd673648eb59459
Mathias Bynens <mathias@qiwi.be>
2018-02-21 09:54:52 -0800

    Update identifier data to ES2015+ and Unicode 10

--- scripts/generate-identifier-data.js
+++ scripts/generate-identifier-data.js
@@ -5,40 +5,35 @@
 var regenerate = require('regenerate');
 
 // Which Unicode version should be used?
-var version = '6.3.0'; // note: also update `package.json` when this changes
+var pkg = require('../package.json');
+var dependencies = Object.keys(pkg.devDependencies);
+var unicodeVersion = dependencies.find((name) => /^unicode-\d/.test(name));
 
 // Shorthand function
 var get = function(what) {
-  return require('unicode-' + version + '/' + what + '/code-points');
+  return require(unicodeVersion + '/' + what + '/code-points.js');
 };
 
-// Unicode categories needed to construct the ES5 regex
-var Lu = get('categories/Lu');
-var Ll = get('categories/Ll');
-var Lt = get('categories/Lt');
-var Lm = get('categories/Lm');
-var Lo = get('categories/Lo');
-var Nl = get('categories/Nl');
-var Mn = get('categories/Mn');
-var Mc = get('categories/Mc');
-var Nd = get('categories/Nd');
-var Pc = get('categories/Pc');
+// Get the Unicode properties needed to construct the ES6 regex.
+var ID_Start = get('Binary_Property/ID_Start');
+var ID_Continue = get('Binary_Property/ID_Continue');
+var Other_ID_Start = get('Binary_Property/Other_ID_Start');
 
-var generateES5Data = function() { // ES 5.1
+var generateData = function() { // ES 5.1
   // http://mathiasbynens.be/notes/javascript-identifiers#valid-identifier-names
-  var identifierStart = regenerate('$', '_')
-    .add(Lu, Ll, Lt, Lm, Lo, Nl)
-    .removeRange(0x010000, 0x10FFFF) // remove astral symbols
-    .removeRange(0x0, 0x7F); // remove ASCII symbols (JSHint-specific)
-  var identifierStartCodePoints = identifierStart.toArray();
-  var identifierPart = regenerate()
-    .add(0x200C, 0x200D, Mn, Mc, Nd, Pc)
-    // remove astral symbols
+  var identifierStart = regenerate(ID_Start)
+    .add('$', '_')
+    // remove astral symbols (JSHint-specific; lex.js needs updating)
     .removeRange(0x010000, 0x10FFFF)
+    .removeRange(0x0, 0x7F); // remove ASCII symbols (JSHint-specific)
+  var identifierPart = regenerate(ID_Continue)
+    .add('$', '_', '\u200C', '\u200D')
     // remove ASCII symbols (JSHint-specific)
     .removeRange(0x0, 0x7F)
+    // remove astral symbols (JSHint-specific; lex.js needs updating)
+    .removeRange(0x010000, 0x10FFFF)
     // just to make sure no `IdentifierStart` code points are repeated here
-    .remove(identifierStartCodePoints);
+    .remove(identifierStart);
   return {
     'nonAsciiIdentifierStart': identifierStart.toArray(),
     'nonAsciiIdentifierPart': identifierPart.toArray()
@@ -59,6 +54,6 @@ var writeFile = function(fileName, data) {
   );
 };
 
-var result = generateES5Data();
+var result = generateData();
 writeFile('./data/non-ascii-identifier-start.js', result.nonAsciiIdentifierStart);
 writeFile('./data/non-ascii-identifier-part-only.js', result.nonAsciiIdentifierPart);
