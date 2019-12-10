8a205a808582579592d5301bba587894e3a824c8
Matt Surabian <matt@mattsurabian.com>
2019-12-09 22:59:57 -0500

    Increase complexity count when encountering the ternary operator

--- src/jshint.js
+++ src/jshint.js
@@ -2418,6 +2418,7 @@ var JSHINT = (function() {
   }, 10, true);
 
   infix("?", function(context, left, that) {
+    increaseComplexityCount();
     that.left = left;
     that.right = expression(context & ~prodParams.noin, 10);
     advance(":");
