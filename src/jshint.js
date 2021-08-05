5d88afaef13d51090aba45d8165953313cae87b7
Mike Pennisi <mike@mikepennisi.com>
2021-08-05 19:55:27 -0400

    [[FIX]] Allow optional chaining call as satement
    Take care to only disable warning W030 for the "function call" form of
    the optional chaining feature; the warning is still relevant for
    property access.

--- src/jshint.js
+++ src/jshint.js
@@ -3070,6 +3070,7 @@ var JSHINT = (function() {
       that.left = left;
       advance();
       that.right = state.tokens.curr.led(context, left);
+      that.exps = true;
     } else {
       state.syntax["."].led.call(that, context, left);
     }
